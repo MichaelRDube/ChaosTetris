8374c809cda369d73b0d3fcf9c89ccecd363046b
Michael <michael_dube@mymail.eku.edu>
2023-12-03 20:24:54 -0500

    Added update script

---
+++ Assets/update script.py
@@ -0,0 +1,12 @@
+import urllib.request, sys, os
+
+#if not os.path.isdir("Assets"):
+#    os.mkdir("Assets")
+    
+#bring in assets
+urllib.request.urlretrieve('https://raw.github.com/MichaelRDube/ChaosTetris/main/Assets/game_over.png', 'game_over.png')
+urllib.request.urlretrieve('https://raw.github.com/MichaelRDube/ChaosTetris/main/Assets/score.png', 'score.png')
+urllib.request.urlretrieve('https://raw.github.com/MichaelRDube/ChaosTetris/main/Assets/wow.png', 'wow.png')
+
+#bring in the executable
+urllib.request.urlretrieve('https://raw.github.com/MichaelRDube/ChaosTetris/main/Minimal%20Package/Tetris.exe', '../Tetris.exe')
