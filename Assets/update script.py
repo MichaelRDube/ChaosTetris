f846e40af0bd97b5bac08bb6a48391f1333b5b18
Michael <michael_dube@mymail.eku.edu>
2023-12-05 19:14:51 -0500

    There was nothing wrong with the update script, I guess?

--- Assets/update script.py
+++ Assets/update script.py
@@ -4,26 +4,26 @@ import urllib.request, sys, os
 #    os.mkdir("Assets")
     
 #bring in assets
-#print("Fetching assets...")
-#print("\tFetching game_over.png...")
+print("Fetching assets...")
+print("\tFetching game_over.png...")
 urllib.request.urlretrieve('https://raw.github.com/MichaelRDube/ChaosTetris/main/Assets/game_over.png', 'Assets/game_over.png')
-#print("\tDone.")
-#print("\tFetching score.png...")
+print("\tDone.")
+print("\tFetching score.png...")
 urllib.request.urlretrieve('https://raw.github.com/MichaelRDube/ChaosTetris/main/Assets/score.png', 'Assets/score.png')
-#print("\tDone.")
-#print("\tFetching wow.png...")
+print("\tDone.")
+print("\tFetching wow.png...")
 urllib.request.urlretrieve('https://raw.github.com/MichaelRDube/ChaosTetris/main/Assets/wow.png', 'Assets/wow.png')
-#print("\tDone.")
+print("\tDone.")
 
 if not os.path.isfile('Assets/high_scores.txt'):
-    #print("\tFetching high_scores.png...")
+    print("\tFetching high_scores.png...")
     urllib.request.urlretrieve('https://raw.github.com/MichaelRDube/ChaosTetris/main/Assets/high_scores.txt', 'Assets/high_scores.txt')
-    #print("\tDone.")
-#else:
-    #print("\thigh_scores.txt already exists in assets.")
+    print("\tDone.")
+else:
+    print("\thigh_scores.txt already exists in assets.")
     
-#print("Fetching executables...")
-#print("\tFetching Tetris.exe...")
+print("Fetching executables...")
+print("\tFetching Tetris.exe...")
 #bring in the executable
 urllib.request.urlretrieve('https://raw.github.com/MichaelRDube/ChaosTetris/main/dist/Tetris.exe', 'Tetris.exe')
-#print("Done.")
+print("Done.")
